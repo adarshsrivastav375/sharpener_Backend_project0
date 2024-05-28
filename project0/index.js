247e86b93761644cf94c394bfb466adf5d36cf95
Adarsh srivastav <adarshsrivastav375@gmail.com>
2024-05-28 16:31:21 +0530

    student Object created

--- project0/index.js
+++ project0/index.js
@@ -4,3 +4,11 @@ console.log("hello world");
 const product = (a, b) => a * b;
 console.log(product(2, 8))
 
+//2. Student object
+
+const student = {
+    name: "John",
+    age: "23",
+    greet() { console.log(`hi! I'm ${this.name}. I'm ${this.age} year old.`); }
+}
+student.greet();
