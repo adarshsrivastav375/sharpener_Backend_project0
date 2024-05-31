2d3a8e2a31956545a0c9fec584417c9b53efc371
Adarsh srivastav <adarshsrivastav375@gmail.com>
2024-05-31 14:15:54 +0530

    routing

--- project0/server.js
+++ project0/server.js
@@ -1,13 +1,33 @@
 const http = require("http");
 const port = 4000;
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers, req.method);
+  const url = req.url;
   res.setHeader("Content-Type", "text/html");
-  res.write(" <html> ");
-  res.write("<head><title> first-server-page </title></head>");
-  res.write(" <body><h1> Hi Adarsh Srivastav</h1></body>");
-  res.write("</html > ");
-  res.end();
+  if (url === "/home") {
+    res.write(" <html> ");
+    res.write("<head><title> first-server-page </title></head>");
+    res.write(" <body><h1> welcome Home</h1></body>");
+    res.write("</html > ");
+    res.end();
+  } else if (url === "/about") {
+    res.write(" <html> ");
+    res.write("<head><title> first-server-page </title></head>");
+    res.write(" <body><h1> welcome to About us page</h1></body>");
+    res.write("</html > ");
+    return res.end();
+  } else if (url === "/node") {
+    res.write(" <html> ");
+    res.write("<head><title> first-server-page </title></head>");
+    res.write(" <body><h1> welcome to my node js project</h1></body>");
+    res.write("</html > ");
+    return res.end();
+  } else {
+    res.write(" <html> ");
+    res.write("<head><title> first-server-page </title></head>");
+    res.write(" <body><h1> Hi Adarsh</h1></body>");
+    res.write("</html > ");
+    return res.end();
+  }
 });
 server.listen(port, () => {
   console.log("server is running at ", port);
