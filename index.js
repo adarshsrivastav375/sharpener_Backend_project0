27cf01eb03326d39359c66e77a54b631f9c18fa7
Adarsh srivastav <adarshsrivastav375@gmail.com>
2024-06-03 16:15:19 +0530

    nodemon setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
-const handler = require("./routes");
+const handler = require("./src/routes");
 const port = 4000;
 const server = http.createServer(handler);
 server.listen(port, () => {
-  console.log(`server is running at ${port}`);
+  console.log(` my server is running at ${port}`);
 });
